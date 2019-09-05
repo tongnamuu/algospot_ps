e2bbd7ec134ee6d77d3a30697e023f06e6491334
tongnamuu <tongnamuu>
2019-09-05 23:02:03 +0900

    CHAPTER19 BRACKETS2

---
+++ BRACKETS2/BRACKETS2.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+char a[10001];
+char stack[10001];
+int pn;
+void init() { pn = 0; }
+void push(char x) { stack[pn++] = x; }
+char top() { return stack[pn - 1]; }
+void pop() { if (pn > 0) --pn; }
+bool empty() { return pn == 0; }
+int main() {
+	ios_base::sync_with_stdio(false), cin.tie(0), cout.tie(0);
+	int T; cin >> T;
+	while (T--) {
+		init();
+		bool ans = true;
+		cin >> a;
+		for (int i = 0; a[i]; ++i) {
+			if (a[i] == '(' || a[i] == '{' || a[i] == '[') {
+				push(a[i]);
+			}
+			else {
+				if (a[i] == ')') {
+					if (top() != '(') {
+						ans = false;
+						break;
+					}
+					pop();
+				}
+				else if (a[i] == '}') {
+					if (top() != '{') {
+						ans = false;
+						break;
+					}
+					pop();
+				}
+				else if (a[i] == ']') {
+					if (top() != '[') {
+						ans = false;
+						break;
+					}
+					pop();
+				}
+			}
+		}
+		if (!empty()) ans = false;
+		if (ans) cout << "YES" << '\n';
+		else cout << "NO" << '\n';
+	}
+}
